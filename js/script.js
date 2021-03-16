0acff1677ba941892d83ae7a7cfce8af103e5f4b
aden-i <79594613+aden-i@users.noreply.github.com>
2021-03-16 17:19:18 +0600

    Aded Music Part

--- js/script.js
+++ js/script.js
@@ -1,17 +1,60 @@
 
 $(document).ready(function () {
     let audio = document.getElementById('audio');
+    let audio2 = document.getElementById('audio2');
+    let audio3 = document.getElementById('audio3');
+    let audio4 = document.getElementById('audio4');
     let isPlay = false ;
 
-    $('.player__btn').click(function(){
-        $('.player__innerBtn').css('margin-left:', '20px;' )
+    $('.ouPrj__playStopBtn--1').click(function(){
 
         if (isPlay == false) {
             isPlay = true;
             audio.play() ;
+            $('.ouPrj__btnIco--1').addClass('pause');
         }else {
             isPlay = false;
             audio.pause() ;
+            $('.ouPrj__btnIco--1').removeClass('pause');
+        }
+    });
+
+    $('.ouPrj__playStopBtn--2').click(function(){
+
+        if (isPlay == false) {
+            isPlay = true;
+            audio2.play() ;
+            $('.ouPrj__btnIco--2').addClass('pause');
+        }else {
+            isPlay = false;
+            audio2.pause() ;
+            $('.ouPrj__btnIco--2').removeClass('pause');
+        }
+    });
+    
+    $('.ouPrj__playStopBtn--3').click(function(){
+
+        if (isPlay == false) {
+            isPlay = true;
+            audio3.play() ;
+            $('.ouPrj__btnIco--3').addClass('pause');
+        }else {
+            isPlay = false;
+            audio3.pause() ;
+            $('.ouPrj__btnIco--3').removeClass('pause');
+        }
+    });
+
+    $('.ouPrj__playStopBtn--4').click(function(){
+
+        if (isPlay == false) {
+            isPlay = true;
+            audio4.play() ;
+            $('.ouPrj__btnIco--4').addClass('pause');
+        }else {
+            isPlay = false;
+            audio4.pause() ;
+            $('.ouPrj__btnIco--4').removeClass('pause');
         }
     });
  });
